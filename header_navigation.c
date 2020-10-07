24cb45d40a95e03a74af410bb77d589a510a5e33
icicle <>
2020-10-07 18:41:42 +0200

    Add even more helper functions

--- header_navigation.c
+++ header_navigation.c
@@ -11,3 +11,39 @@ struct Header *get_prev_header(struct Header *header)
     header = (char *)header - header->prev - sizeof(struct Header);
     return header;
 }
+
+bool is_edge_header(struct Header *header)
+{
+    if (header->busy == true && header->size == 0)
+        return true;
+    else
+        return false;
+}
+
+bool allocate_segment(size_t req_size, struct Header *header)
+{
+    if (header->size >= req_size)
+    {
+        // split into two segments if enough space; else allocate entire
+        if (header->size > req_size + sizeof(struct Header))
+        {
+            // initialize succeeding header
+            struct Header *next_header = (char *)header + req_size +
+                                         sizeof(struct Header);
+            next_header->busy = false;
+            next_header->size = header->size - req_size - sizeof(struct Header);
+            next_header->prev = req_size;
+
+            // readjust header size
+            header->size = req_size;
+        }
+        
+        header->busy = true;
+
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
--- header_navigation.h
+++ header_navigation.h
@@ -1,9 +1,13 @@
 #ifndef HEADER_NAVIGATION_H
 #define HEADER_NAVIGATION_H
 
+#include <stdbool.h>
+
 #include "constants.h"
 
 struct Header *get_next_header(struct Header *);
 struct Header *get_prev_header(struct Header *);
+bool is_edge_header(struct Header *);
+bool allocate_segment(size_t, struct Header *);
 
 #endif
