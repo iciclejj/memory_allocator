7af602e9b67e5fd75094c68fdc3001e8d99a55ff
icicle <>
2020-10-07 18:43:29 +0200

    Rework mem_alloc.c to use helper functions

--- mem_alloc.c
+++ mem_alloc.c
@@ -2,51 +2,22 @@
 #include <stdbool.h>
 
 #include "constants.h"
+#include "header_navigation.h"
 
 void *mem_alloc(size_t req_size, void *init_addr)
 {
-    struct Header *curr_addr = init_addr;
+    struct Header *curr_header = init_addr;
 
-    // loop until you reach the end bound or until break
-    while (!(curr_addr->busy == true &&
-             curr_addr->size == 0))
+    while (!is_edge_header(curr_header))
     {
-        // check if currently checked segment is free and is big enough
-        if (curr_addr->busy == false &&
-            curr_addr->size >= req_size)
-        {
-            // split into two segments if enough space
-            // else allocate entire segment
-            if (curr_addr->size > req_size + sizeof(struct Header))
-            {
-                // init succeeding segment
-                struct Header *next_addr = (char *)curr_addr + req_size + 
-                                           sizeof(struct Header);
-                next_addr->busy = false;
-                next_addr->size = curr_addr->size - req_size - 
-                                  sizeof(struct Header);
-                next_addr->prev = req_size;
-
-                // readjust size of selected segment
-                curr_addr->size = req_size;
-            }
-
+        if (allocate_segment(req_size, curr_header))
             break;
-        }
-
-        // get address of next segment to check
-        curr_addr = (char *)curr_addr + curr_addr->size + sizeof(struct Header);
+        else
+            curr_header = get_next_header(curr_header);
     }
 
-    // return NULL if busy segment was chosen (end bound header)
-    // else set segment as busy and return pointer to the non-header portion
-    if (curr_addr->busy == true)
-    {
+    if (is_edge_header(curr_header))
         return NULL;
-    }
     else
-    {
-        curr_addr->busy = true;
-        return curr_addr + 1;
-    }
+        return curr_header + 1;
 }
